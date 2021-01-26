20abe57081d59bf429058c8d8b4c23cd4be8cba8
Allie <alliecaton3@gmail.com>
2021-01-26 14:54:53 -0500

    commit tree 836ecaa7813d3db72b6fbf5fd20d4f97e68d6941

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+
+    get '/' do 
+        erb :super_hero
+    end 
+
+    post '/teams' do 
+        @team = Team.new(params[:team][:name], params[:team][:motto])
+        
+        params[:team][:members].each do |member|
+            Member.new(member[:name], member[:power], member[:bio])
+        end
+
+        erb :team
+
+    end
 
 
 end
